2490d9dbcb3c6e140cc9c6224a0f22d8a6981600
danielfmonzon <123423019+danielfmonzon@users.noreply.github.com>
2024-02-01 16:01:48 -0500

    Update Self_Attention.py

--- example_prediction/Self_Attention.py
+++ example_prediction/Self_Attention.py
@@ -1,6 +1,6 @@
 from keras.preprocessing import sequence
 from keras import backend as K
-from keras.engine.topology import Layer
+from tensorflow.keras.layers import Layer
 
 
 class Self_Attention(Layer):
