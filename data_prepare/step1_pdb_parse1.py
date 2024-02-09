d507da5c99cbd3b3ff1249cf3d0d1c637cf732c8
danielfmonzon <123423019+danielfmonzon@users.noreply.github.com>
2024-02-08 19:24:35 -0500

    Update step1_pdb_parse1.py

--- data_prepare/step1_pdb_parse1.py
+++ data_prepare/step1_pdb_parse1.py
@@ -1,25 +1,20 @@
 import pandas as pd
 import numpy as np
 
-# Step 0: parse the fasta file downloaded from the RCSB PDB
-# INPUT : pdb_seqres.txt
-# OUTPUT: pdb_pep_chain, pdbid_all_fasta
-raw_str=''
-with open(r'C:\Users\danmo\Downloads\pdb_seqres.txt\pdb_seqres.txt', 'r') as f:
-    for line in f.readlines():
-        raw_str = raw_str+line.replace('\n','###')
-raw_list = raw_str.split('>')
-del raw_list[0]
+# Your existing code for reading the file and creating raw_list
 
-PDB_id_lst = [x.split('_')[0] for x in raw_list]
-PDB_chain_lst = [x.split('_')[1].split(' ')[0].lower() for x in raw_list]
-PDB_type_lst = [x.split('mol:')[1].split(' ')[0] for x in raw_list]
-PDB_seq_lst = [x.split('###')[1] for x in raw_list]
+# Safe splitting function
+def safe_split(source, separator, index, default=None):
+    parts = source.split(separator)
+    return parts[index] if len(parts) > index else default
+
+# Parse out the desired information with safety checks
+PDB_id_lst = [safe_split(x, '_', 0) for x in raw_list]
+PDB_chain_lst = [safe_split(safe_split(x, '_', 1, ''), ' ', 0, '').lower() for x in raw_list]
+PDB_type_lst = [safe_split(safe_split(x, 'mol:', 1, ''), ' ', 0) for x in raw_list]
+PDB_seq_lst = [safe_split(x, '###', 1) for x in raw_list]
 PDB_seq_len_lst = [len(x) for x in PDB_seq_lst]
-df_fasta_raw =pd.DataFrame(list(zip(PDB_type_lst, PDB_seq_len_lst,PDB_seq_lst,PDB_id_lst,PDB_chain_lst)),\
-                       columns=['PDB_type','PDB_seq_len','PDB_seq','PDB_id','chain'])
-df_fasta = df_fasta_raw[(df_fasta_raw.PDB_seq_len<=50)&(df_fasta_raw.PDB_type=='protein')]
-df_fasta_raw.to_csv(r'C:\Users\danmo\Downloads\pdbid_all_fasta.tsv', encoding='utf-8', index=False, sep = '\t')
-df_fasta.to_csv(r'C:\Users\danmo\Downloads\pdb_pep_chain.tsv', encoding='utf-8', index=False, sep = '\t')
+
+# Your existing code for creating the DataFrame and saving it to CSV
 
 print('Step 0 is finished by generating two files : pdb_pep_chain & pdbid_all_fasta!')
